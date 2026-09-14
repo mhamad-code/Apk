cac036b5e36023b226392eb837a0103bc04f903c
mhamad-code <mhamra148@gmail.com>
2026-09-15 00:11:24 +0300

    Fix missing getValue import in RamDetailScreen

--- app/src/main/java/com/devicex/app/screens/RamDetailScreen.kt
+++ app/src/main/java/com/devicex/app/screens/RamDetailScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -92,7 +93,6 @@ fun RamDetailScreen(navController: NavHostController) {
                     modifier = Modifier.padding(bottom = 8.dp)
                 )
 
-                // شريط استخدام مخصص: خلفية غامقة + تعبئة بتدرج ذهبي، مو progress bar افتراضي فلات
                 Box(
                     modifier = Modifier
                         .fillMaxWidth()
